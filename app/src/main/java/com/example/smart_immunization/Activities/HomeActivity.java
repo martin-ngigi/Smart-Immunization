9acac484df392386376f2e432a36a79a4a65a207
martin-ngigi <martinwainaina001@gmail.com>
2022-10-23 10:19:43 +0300

    Added Immunization schedules

--- app/src/main/java/com/example/smart_immunization/Activities/HomeActivity.java
+++ app/src/main/java/com/example/smart_immunization/Activities/HomeActivity.java
@@ -2,15 +2,25 @@ package com.example.smart_immunization.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.smart_immunization.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private LinearLayout immuneLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        immuneLayout = findViewById(R.id.immuneLayout);
+
+        immuneLayout.setOnClickListener(e ->{
+            startActivity(new Intent(HomeActivity.this, ImmunizationActivity.class));
+        });
     }
 }
---
+++ app/src/main/java/com/example/smart_immunization/Activities/ImmunizationActivity.java
@@ -0,0 +1,18 @@
+package com.example.smart_immunization.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smart_immunization.R;
+
+public class ImmunizationActivity extends AppCompatActivity {
+
+    //link https://my.clevelandclinic.org/health/articles/11288-childhood-immunization-schedule
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_immunization);
+    }
+}
